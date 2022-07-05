b7cdca78920cfb9f84d25cf70a55b91ccba22ea0
sudha74 <107973435+sudha74@users.noreply.github.com>
2022-07-05 13:36:39 +0530

    Add files via upload

---
+++ TwoDimension.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+public class TwoDimension {
+    // java program to print the elements of
+    // a 2 D aaray or matrix
+    import java.io.*;
+    class GFG {
+
+        public static void print2D(int mat[][])
+        {
+            // Loop through all rows
+            for (int i = 0; i<mat.length; i++)
+                // Loop through elements of all elements of current row
+                for (int j = 0; j<mat[i].length; j++)
+                    System.out.println(mat[i][j] + " " );
+        }
+        public static void main(String [] args)
+            throws IOException
+        {
+            int mat[][]= { { 1,2,3,4 },{5,6,7,8 },{9,10,11,12 } }
+
+
+
+            print2D(mat);
+        }
+    }
+
+
